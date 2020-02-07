a7c5531d4769a3bf28a1fef180e4cc1ec393afce
nsutt23 <nhsutton@ou.edu>
2020-02-07 15:49:47 -0600

    Commit #2

---
+++ R/Package1.R
@@ -0,0 +1,16 @@
+#' Add together two numbers.
+#'
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+functionname <- function(x)
+  myread=function(csv){
+    fl=paste(dird,csv,sep="")
+    read.table(fl,header=TRUE,sep=",")
+}
+
+#' This function is from Lab 2 and its function is to take the date from the dataset csv and show it within this document.
+
